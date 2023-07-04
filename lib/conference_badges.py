a439898ac92a5c299892b1b91ed96ae2c4e49902
Wesley Peters <wesleypeters@gmail.com>
2023-07-04 14:28:47 -0600

    write functions to pass tests using list comprehensions

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,13 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
+
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {i+1}!" for i, name in enumerate(names)]
 
 def printer(names):
-    return None
+    [print(badge) for badge in batch_badge_creator(names)]
+    [print(assignment) for assignment in assign_rooms(names)]
